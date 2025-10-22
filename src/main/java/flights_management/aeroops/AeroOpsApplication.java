3f737c0876629a3eee74aaa7c70e0d635dccae88
AlexandruStefu <alexandru.stefu@endava.com>
2025-10-22 16:45:49 +0300

    Innit

---
+++ src/main/java/flights_management/aeroops/AeroOpsApplication.java
@@ -0,0 +1,13 @@
+package flights_management.aeroops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroOpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeroOpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/flights_management/aeroops/AeroOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package flights_management.aeroops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
