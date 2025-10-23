a359365303c1bcb58167a5fc02d70c0d82b5ac24
AlexandruStefu <alexandru.stefu@endava.com>
2025-10-23 09:16:18 +0300

    test: enable PostgreSQL tests via Testcontainers and run in CI

--- src/test/java/flights_management/aeroops/AeroOpsApplicationTests.java
+++ src/test/java/flights_management/aeroops/AeroOpsApplicationTests.java
@@ -1,13 +1,10 @@
 package flights_management.aeroops;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AeroOpsApplicationTests extends IntegrationTestBase {
 
     @Test
     void contextLoads() {
     }
-
 }
