3993bae8e5a141865e10ce09b7b3de6f740cd223
AlexandruStefu <alexandru.stefu@endava.com>
2025-10-31 13:35:22 +0200

    Endpoints added for creating and listing flights

---
+++ src/main/java/flights_management/aeroops/dto/flight/FlightDTO.java
@@ -0,0 +1,19 @@
+package flights_management.aeroops.dto.flight;
+
+import flights_management.aeroops.enums.Status;
+
+import java.time.ZonedDateTime;
+
+
+// Pentru referinte in alte DTO-uri
+
+public record FlightDTO(
+        Long id,
+        String flightNumber,
+        String airlineName,
+        String originIata,
+        String destinationIata,
+        ZonedDateTime scheduledDeparture,
+        ZonedDateTime scheduledArrival,
+        Status status
+) { }
