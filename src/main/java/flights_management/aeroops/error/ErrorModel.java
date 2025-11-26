577869e887333d871301670430e6edd5a0046473
Stefu Alexandru <93092264+StefuAlexandru@users.noreply.github.com>
2025-11-26 11:29:03 +0200

    Name fixes and ErrorModel default constructor added (#22)
    Co-authored-by: AlexandruStefu <alexandru.stefu@endava.com>

--- src/main/java/flights_management/aeroops/error/ErrorModel.java
+++ src/main/java/flights_management/aeroops/error/ErrorModel.java
@@ -2,9 +2,11 @@ package flights_management.aeroops.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorModel {
     public String code;
     public String message;
--- src/main/java/flights_management/aeroops/repository/FlightRepository.java
+++ src/main/java/flights_management/aeroops/repository/FlightRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FlightsRepository extends JpaRepository<Flight,Long> { }
+public interface FlightRepository extends JpaRepository<Flight,Long> { }
