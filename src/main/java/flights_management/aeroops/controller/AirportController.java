efddafcc357a21a5c2511f814d62bbb99c116c4c
andr3iyw <popoviciandrei420@gmail.com>
2025-12-03 11:32:32 +0200

    fixes, added dtos

--- src/main/java/flights_management/aeroops/controller/AirportController.java
+++ src/main/java/flights_management/aeroops/controller/AirportController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/airports")
 @RequiredArgsConstructor
-public class AirportsController {
+public class AirportController {
 
     private final IAirportService airportService;
 
